3f5c0208c3726c0163dfbe406473c4ef6c20a74a
ricieri <ricieri.junior@escola.pr.gov.br>
2026-06-16 12:27:55 -0300

    l~kj

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function carregar(){
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
      
-    var data = new Date ()
+    var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
 
