3feb204cd7a1ddc3f132259616f104bc8e7a0fff
ricieri <ricieri.junior@escola.pr.gov.br>
2026-06-16 12:32:53 -0300

    DADADA

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
 //O JavaScript é uma linguagem WEB
 
-//Variáveis - Valores armazenados na memória do computador
+//Variaveis - Valores armazenados na memória do computador
 
-//Funções - Blocos de código 
+//Funções - Blocos de código
 
-function carregar(){
-    //alert("Bem vindo ao JavaScript")
+function carregar() {
+    //alert("Bem Vindo!")
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
-     
+
     var data = new Date()
     var hora = data.getHours()
     var minutos = data.getMinutes()
 
-    msg.innerHTML = `agora sao ${hora}:${minutos}am`
+    msg.innerHTML = `Agora são ${hora}:${minutos}`
 
-    if(hora >=0 && hora <12){
+    if (hora >= 0 && hora < 12) {
         img.src = 'manha.png'
         document.body.style.background = '#A0CFD3'
-    }
-    else if (hora >12 && hora <18){
+    } else if (hora >= 12 && hora < 18) {
         img.src = 'tarde.png'
         document.body.style.background = '#023047'
-    }
-    else{
-         img.src = 'noite.png'
+    } else {
+        img.src = 'noite.png'
         document.body.style.background = '#001219'
-
     }
 }
